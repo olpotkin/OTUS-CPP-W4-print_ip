c75593b3f906fb9a174dd511e318db8f70c53446
olpotkin <olpotkin@gmail.com>
2019-03-11 20:10:50 +0100

    feat: Print IP represented as std::string type

--- core/print_ip.h
+++ core/print_ip.h
@@ -1,20 +1,42 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 
-
-/// @brief  Prints IP represented in integral type
 /// References:
 /// - https://en.cppreference.com/w/cpp/types/enable_if
 /// - https://www.daniweb.com/programming/software-development/threads/200708/how-to-convert-uint32-to-ip-address-dot-format
+/// - http://www.cplusplus.com/reference/type_traits/is_integral/
+/// - http://www.cplusplus.com/reference/type_traits/integral_constant/
+/// Helper types
+/// template< bool B, class T = void >
+/// using enable_if_t = typename enable_if<B,T>::type;
+
+
+/// @brief  Prints IP represented as integral type
 /// @tparam T - integral type (bool, char, short, uint, int, long, etc...)
 /// @param  ip - IP address
-/// @author olpotkin@gmail.com
+/// @author Oleg Potkin <olpotkin@gmail.com>
 template <typename T>
-typename std::enable_if<std::is_integral<T>::value> print_ip(const T& ip) {
+typename std::enable_if_t<std::is_integral<T>::value> print_ip(const T& ip) {
   for (size_t sz = sizeof(T); sz != 0; --sz) {
     std::cout << ((ip >> (8 * (sz - 1))) & 0xff)
       << (sz != 1 ? "." : "");
   }
-  std::cout << std::endl;
+}
+
+
+template <typename T>
+struct is_string : std::false_type {};
+
+template <>
+struct is_string<std::string> : std::true_type {};
+
+/// @brief  Prints IP represented as std::string type
+/// @tparam T - std::string type
+/// @param  ip - IP address
+/// @author Oleg Potkin <olpotkin@gmail.com>
+template <typename T>
+typename std::enable_if<is_string<T>::value>::type print_ip(const T& ip) {
+  std::cout << ip;
 }
--- main.cpp
+++ main.cpp
@@ -2,22 +2,22 @@
 
 
 /// @brief  Main function. Prints IP addresses in multiple formats
-/// @author olpotkin@gmail.com
+/// @author Oleg Potkin <olpotkin@gmail.com>
 int main() {
-  std::cout << "Integral: char" << std::endl;
   print_ip(char(-1));
-  std::cout << "---" << std::endl;
+  std::cout << std::endl;
 
-  std::cout << "Integral: short" << std::endl;
   print_ip(short(0));
-  std::cout << "---" << std::endl;
+  std::cout << std::endl;
 
-  std::cout << "Integral: int" << std::endl;
   print_ip(int(2130706433));
-  std::cout << "---" << std::endl;
+  std::cout << std::endl;
 
-  std::cout << "Integral: long" << std::endl;
   print_ip(long(8875824491850138409));
-  std::cout << "---" << std::endl;
+  std::cout << std::endl;
+
+  print_ip(std::string("192.168.1.1"));
+  std::cout << std::endl;
+
   return 0;
 }
